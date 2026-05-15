3ab337d5519483f95b9d07cf5576a8fed82e1ea9
chaewon021734 <zeta01873@gmail.com>
2026-05-15 18:41:10 +0900

    스프린트 미션1 - 1차 완

---
+++ src/main/java/com/sprint/mission/discodeit/entity/Channel.java
@@ -0,0 +1,40 @@
+package com.sprint.mission.discodeit.entity;
+
+import java.util.UUID;
+
+import static java.lang.System.currentTimeMillis;
+
+public class Channel {
+    private UUID id;
+    private Long createdAt;
+    private Long updatedAt;
+    private String room;
+
+    public Channel(String room) {
+        this.id = UUID.randomUUID();
+        this.createdAt = currentTimeMillis();
+        this.updatedAt = createdAt;
+        this.room = room;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public Long getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public Long getCreatedAt() {
+        return createdAt;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void update(String room) {
+        this.room = room;
+        this.updatedAt = currentTimeMillis();
+    }
+}
